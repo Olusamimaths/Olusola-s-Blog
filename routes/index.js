fe38cc205e5f157cf3d49b2723077d4f7f42a2bf
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2018-12-22 20:34:10 +0100

    Fix for homepage

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const mongo = require('mongodb')
-var monk = require('monk');
-var db =  monk('mongodb://admin:bestAdmin109@ds141924.mlab.com:41924/nodeblog'); 
+const monk = require('monk');
+const db =  monk('mongodb://admin:bestAdmin109@ds141924.mlab.com:41924/nodeblog'); 
 
 
-var router = express.Router();
+const router = express.Router();
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var db = req.db;
-	var posts = db.get('posts');
+	let posts = db.get('posts');
 	posts.find({}, {sort : { date : -1 }}, function(err, posts){
 		res.render('index', {title:'Olusola\'s Blog', posts, });
 	});
